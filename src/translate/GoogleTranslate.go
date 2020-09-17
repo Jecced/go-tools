565ed0267cd76b9c02f67a5de83bf439799bb1a7
Jecced <jecced@gmail.com>
2020-09-17 17:15:23 +0800

    翻译返回结构重构

--- src/translate/GoogleTranslate.go
+++ src/translate/GoogleTranslate.go
@@ -1,6 +1,8 @@
 package translate
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/Jecced/rs/src/rs"
 	"net/url"
@@ -16,20 +18,44 @@ import (
 func GoogleTranslate(text string) string {
 	uri := "https://translate.google.cn/"
 
-	resp := rs.Get(uri).Send().ReadText()
+	resp := rs.Get(uri).
+		SetTimeOut(600_000).
+		//Proxy("127.0.0.1:1081").
+		Send().
+		ReadText()
 	tkk := getSubText(resp, "tkk:'", "'")
 	xid := getSubText(resp, "triggered_experiment_ids:[", "]")
 
-	translateUri := "https://translate.google.cn/translate_a/single?client=webapp&sl=zh-CN&tl=en&hl=zh-CN&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=sos&dt=ss&dt=t&ssel=6&tsel=3&xid=%s&kc=0&tk=%s&q=%s"
+	//translateUri := "https://translate.google.cn/translate_a/single?client=webapp&sl=en&tl=zh-CN&hl=zh-CN&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=sos&dt=ss&dt=t&pc=1&otf=1&ssel=3&tsel=6&xid=45662847&kc=1&tk=886133.740610&q=%22Clearly%2C%20then%2C%20the%20city%20is%20not%20a%20concrete%20jungle%2C%20it%20is%20a%20human%20zoo.%22"
+	translateUri := "https://translate.google.cn/translate_a/single?client=webapp&sl=en&tl=zh-CN&hl=zh-CN&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=sos&dt=ss&dt=t&&ssel=6&tsel=3&xid=%s&kc=0&tk=%s&q=%s"
 	//text := "今天天气很不错"
 
 	tks := tk(text, tkk)
 
-	resp = rs.Get(fmt.Sprintf(translateUri, xid, tks, url.QueryEscape(text))).Send().ReadText()
-
-	out := getSubText(resp, "[[[\"", "\"")
-	//fmt.Println(i, out)
+	resp = rs.Get(fmt.Sprintf(translateUri, xid, tks, url.QueryEscape(text))).
+		//Proxy("127.0.0.1:1081").
+		SetTimeOut(600_000).
+		Send().
+		ReadText()
+	out := format(resp)
 	return out
+}
+
+func format(str string) string {
+	//fmt.Println(resp)
+	var wo []interface{}
+	_ = json.Unmarshal([]byte(str), &wo)
+	var o = wo[0]
+	buff := bytes.Buffer{}
+	for _, v := range o.([]interface{}) {
+		c := v.([]interface{})
+		if c[0] == nil {
+			break
+		}
+		buff.WriteString(c[0].(string))
+		buff.WriteString(" ")
+	}
+	return buff.String()
 }
 
 // 获取截取文本
