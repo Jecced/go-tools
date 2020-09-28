bec9d259d9d724ea7e8056425669795d984ed2e0
Jecced <jecced@gmail.com>
2020-09-28 16:13:01 +0800

    新增下划线转驼峰

--- src/strutil/StringUtil.go
+++ src/strutil/StringUtil.go
@@ -2,14 +2,34 @@ package strutil
 
 import (
 	"regexp"
+	"strings"
 	"unicode"
 )
 
 func IsChineseChar(str string) bool {
 	for _, r := range str {
-		if unicode.Is(unicode.Scripts["Han"], r) || (regexp.MustCompile("[\u3002\uff1b\uff0c\uff1a\u201c\u201d\uff08\uff09\u3001\uff1f\u300a\u300b]").MatchString(string(r))) {
+		if unicode.Is(unicode.Scripts["Han"], r) ||
+			regexp.MustCompile(
+				"[\u3002\uff1b\uff0c\uff1a\u201c\u201d\uff08\uff09\u3001\uff1f\u300a\u300b]",
+			).MatchString(string(r)) {
 			return true
 		}
 	}
 	return false
+}
+
+// 下划线转驼峰
+func UnderscoreToUpperCamelCase(s string) string {
+	s = strings.Replace(s, "_", " ", -1)
+	s = strings.Title(s)
+	return strings.Replace(s, " ", "", -1)
+}
+
+// 下划线转小写驼峰
+func UnderscoreToLowerCamelCase(s string) string {
+	s1 := UnderscoreToUpperCamelCase(s)
+	if s1 == "" {
+		return s
+	}
+	return string(unicode.ToLower(rune(s1[0]))) + s1[1:]
 }
