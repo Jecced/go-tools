69a3938fd28f8303477d743de63134db929779aa
Jecced <jecced@gmail.com>
2020-10-27 15:55:52 +0800

    加入图像翻转

--- src/imgutil/ImageRotation.go
+++ src/imgutil/ImageRotation.go
@@ -2,6 +2,7 @@ package imgutil
 
 import "image"
 
+// img 图像, 顺时针旋转90°
 func RotationRight(m image.Image) *image.RGBA {
 	bounds := m.Bounds()
 	rotate90 := image.NewRGBA(image.Rect(0, 0, bounds.Dy(), bounds.Dx()))
@@ -13,6 +14,7 @@ func RotationRight(m image.Image) *image.RGBA {
 	return rotate90
 }
 
+// img 图像, 逆时针旋转90°
 func RotationLeft(m image.Image) *image.RGBA {
 	bounds := m.Bounds()
 	rotate270 := image.NewRGBA(image.Rect(0, 0, bounds.Dy(), bounds.Dx()))
@@ -24,6 +26,7 @@ func RotationLeft(m image.Image) *image.RGBA {
 	return rotate270
 }
 
+// img 图像, 180°旋转
 func Rotation180(m image.Image) *image.RGBA {
 	bounds := m.Bounds()
 	rotate180 := image.NewRGBA(image.Rect(0, 0, bounds.Dx(), bounds.Dy()))
@@ -35,16 +38,52 @@ func Rotation180(m image.Image) *image.RGBA {
 	return rotate180
 }
 
+// 左右镜像翻转
+func FlipMirror(m image.Image) *image.RGBA {
+	bounds := m.Bounds()
+	out := image.NewRGBA(image.Rect(0, 0, bounds.Dx(), bounds.Dy()))
+	for x := bounds.Min.X; x < bounds.Max.X; x++ {
+		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+			out.Set(bounds.Max.X-x, y, m.At(x, y))
+		}
+	}
+	return out
+}
+
+// 上下垂直翻转
+func FlipVertical(m image.Image) *image.RGBA {
+	bounds := m.Bounds()
+	out := image.NewRGBA(image.Rect(0, 0, bounds.Dx(), bounds.Dy()))
+	for x := bounds.Min.X; x < bounds.Max.X; x++ {
+		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+			out.Set(x, bounds.Max.Y-y, m.At(x, y))
+		}
+	}
+	return out
+}
+
+// img 文件, 顺时针旋转90°
 func RotationImgRight(in, out string) error {
 	return rotation(in, out, RotationRight)
 }
 
+// img 文件, 逆时针旋转90°
 func RotationImgLeft(in, out string) error {
 	return rotation(in, out, RotationLeft)
 }
 
+// img 文件, 180°旋转
 func RotationImg180(in, out string) error {
 	return rotation(in, out, Rotation180)
+}
+
+// img 文件 镜像翻转
+func FlipImgMirror(in, out string) error {
+	return rotation(in, out, FlipMirror)
+}
+
+func FlipImgVertical(in, out string) error {
+	return rotation(in, out, FlipVertical)
 }
 
 func rotation(in, out string, meth func(image.Image) *image.RGBA) error {
