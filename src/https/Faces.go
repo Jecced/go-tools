3d1c1cc143cfaf6c581ed955b12f1939ba7da777
Jecced <jecced@gmail.com>
2020-10-28 15:49:36 +0800

    重命名接口, 加入请求后的构造函数

--- src/https/Faces.go
+++ src/https/Faces.go
@@ -1,14 +1,15 @@
 package https
 
-type Requester interface {
+// 请求前的构造接口
+type Requests interface {
 	// 增加一个参数
-	AddParam(key, value string) Requester
+	AddParam(key, value string) Requests
 	// 增加多个参数
-	AddParams(param map[string]string) Requester
+	AddParams(param map[string]string) Requests
 	// 移除一个参数
-	RemoveParam(key string) Requester
+	RemoveParam(key string) Requests
 	// 清空所有参数
-	ClearParam() Requester
+	ClearParam() Requests
 
 	//// 增加一个请求头
 	//AddHeader(key, value string)
@@ -40,4 +41,14 @@ type Requester interface {
 	//
 	//// 基础认证
 	//BasicAuth(user, password string)
+
+	// 发送
+	//Send() Responses
+}
+
+// 请求后的构造接口
+type Responses interface {
+	Close()
+	WriteFile(path string)
+	ReadText() string
 }
--- src/https/Param.go
+++ src/https/Param.go
@@ -1,7 +1,7 @@
 package https
 
 // 增加一个参数
-func (h *https) AddParam(key, value string) Requester {
+func (h *https) AddParam(key, value string) Requests {
 	get := h.param.Get(key)
 	if "" == get {
 		h.param.Add(key, value)
@@ -12,7 +12,7 @@ func (h *https) AddParam(key, value string) Requester {
 }
 
 // 增加多个请求参数
-func (h *https) AddParams(param map[string]string) Requester {
+func (h *https) AddParams(param map[string]string) Requests {
 	for k, v := range param {
 		h.AddParam(k, v)
 	}
@@ -20,13 +20,13 @@ func (h *https) AddParams(param map[string]string) Requester {
 }
 
 // 移除一个请求参数
-func (h *https) RemoveParam(key string) Requester {
+func (h *https) RemoveParam(key string) Requests {
 	h.param.Del(key)
 	return h
 }
 
 // 清空所有请求参数
-func (h *https) ClearParam() Requester {
+func (h *https) ClearParam() Requests {
 	for k := range h.param {
 		h.RemoveParam(k)
 	}
