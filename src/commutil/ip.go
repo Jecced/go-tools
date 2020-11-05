1a9f2e74fb527d88bfada8df5ea3c6fb5dcf98bd
Jecced <jecced@gmail.com>
2020-11-05 19:56:42 +0800

    从远端机器请求request获取ip

--- src/commutil/ip.go
+++ src/commutil/ip.go
@@ -4,6 +4,12 @@ import (
 	"errors"
 	"github.com/Jecced/go-tools/src/https"
 	"net"
+	"net/http"
+)
+
+const (
+	XForwardedFor = "X-Forwarded-For"
+	XRealIP       = "X-Real-IP"
 )
 
 // 获取本机内网ip
@@ -33,4 +39,21 @@ func GetInternal() (string, error) {
 // 获取本机公网ip
 func GetExternal() (string, error) {
 	return https.Get("https://myexternalip.com/raw").Send().ReadText()
+}
+
+// RemoteIp 返回远程客户端的 IP，如 192.168.1.1
+func RemoteIp(req *http.Request) string {
+	remoteAddr := req.RemoteAddr
+	if ip := req.Header.Get(XRealIP); ip != "" {
+		remoteAddr = ip
+	} else if ip = req.Header.Get(XForwardedFor); ip != "" {
+		remoteAddr = ip
+	} else {
+		remoteAddr, _, _ = net.SplitHostPort(remoteAddr)
+	}
+
+	if remoteAddr == "::1" {
+		remoteAddr = "127.0.0.1"
+	}
+	return remoteAddr
 }
