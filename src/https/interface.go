30a5421427c6b414f23e62f3c059838a546c6e72
安康 <jecced@gmail.com>
2024-07-11 14:02:06 +0800

    feat: 增加一个session第一阶段的接口

---
+++ src/https/interface.go
@@ -0,0 +1,24 @@
+package https
+
+type ISession interface {
+	AddHeader(key string, value string) *p1
+	AddHeaders(entries map[string]string) *p1
+	RemoveHeader(key string) *p1
+	ClearHeader() *p1
+	AddCookie(key string, value string) *p1
+	AddCookies(entries map[string]string) *p1
+	RemoveCookie(key string) *p1
+	ClearCookie() *p1
+	SetTimeOut(time int) *p1
+	SetConnTimeOut(time int) *p1
+	SetRespTimeOut(time int) *p1
+	Proxy(proxy string) *p1
+	BasicAuth(user string, password string) *p1
+	Retry(count uint) *p1
+	SkipSSLVerify(verify bool) *p1
+	Get(uri string) *p2
+	Post(uri string) *p2
+	GetCookies() param
+	GetCookie(key string) string
+	SetCookieSerializationPath(path string) *p1
+}
